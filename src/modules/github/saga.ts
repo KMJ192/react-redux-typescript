242e688c7137d9f29d9c90d6814d9a4244b219e2
KMJ192 <56943526+KMJ192@users.noreply.github.com>
2021-06-11 22:48:10 +0900

    rtr

--- src/modules/github/saga.ts
+++ src/modules/github/saga.ts
@@ -3,8 +3,11 @@ import { call, put, takeEvery } from 'redux-saga/effects';
 import { getUserProfile, GithubProfile } from "../../api/github";
 
 function* getUserProfileSaga(action: ReturnType<typeof getUserProfileAsync.request>){
+    console.log(action);
     try{
+        //call -> first param => 실행할 함수, 함수의 param
         const userProfile: GithubProfile = yield call(getUserProfile, action.payload);
+        //put -> 특정 action dispatch -> store에 dispatch
         yield put(getUserProfileAsync.success(userProfile));
     }catch(e: any){
         yield put(getUserProfileAsync.failure(e));
